7eb60d3e9860b627e07f9fb8a211d0afef860c79
for <nithins674@gmail.com>
2024-10-25 00:38:59 +0100

    Refactor function signatures in CreateGuildFS.hpp for better readability and use of std::filesystem

--- include/CreateGuildFS.hpp
+++ include/CreateGuildFS.hpp
@@ -4,14 +4,20 @@
 #include <string>
 #include <filesystem>
 #include <fstream>
+#include <system_error>
+#include <vector>
+#include <iostream>
+
 
 #include "GetCurrentFolder.hpp"
 #include "nlohmann/json.hpp"
 
 void createGuildStructure(const std::string &projectName);
-void createRootFolder(const std::string& projectName);
-void createSubfolders(const std::string& rootPath);
-void createDepJson(const std::string& rootPath, const std::string& projectName);
-void createMainCpp(const std::string& rootPath, const std::string& projectName);
+bool createRootFolder(const std::filesystem::path &projectName);
+bool createSubfolders(const std::filesystem::path &rootPath);
+bool createDepJson(const std::filesystem::path &rootPath, const std::string &projectName);
+bool createSampleHeaderFiles(const std::filesystem::path &rootPath);
+bool createSampleSourceFiles(const std::filesystem::path &rootPath);
+bool createMainCpp(const std::filesystem::path &rootPath, const std::string &projectName);
 
 #endif // CREATEGUILDFS_HPP
