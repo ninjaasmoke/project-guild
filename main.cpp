53fddb83c9b495b2f07c75e384dccef2baa2445c
for <nithins674@gmail.com>
2024-10-26 02:14:19 +0100

    Refactor parseFlags function to use Flags.h header file for flag parsing

--- main.cpp
+++ main.cpp
@@ -7,55 +7,13 @@
 
 #include "include/CommandInvoker.hpp"
 #include "include/Command.hpp"
-
-struct Flag
-{
-    std::string name;
-    std::string value;
-    bool hasValue; // Explicitly track if flag has a value
-};
-
-std::map<std::string, Flag> parseFlags(int argc, char *argv[])
-{
-    std::map<std::string, Flag> flags;
-
-    // Skip program name (argv[0])
-    for (int i = 1; i < argc; ++i)
-    {
-        // Prevent buffer overruns by checking string validity
-        if (!argv[i])
-            continue;
-
-        std::string arg = argv[i];
-        if (arg.empty())
-            continue;
-
-        if (arg.find("--") == 0)
-        { // Long flag
-            std::string flagName = arg;
-            std::string value;
-            bool hasValue = false;
-
-            // Check if next argument exists and is a value (not a flag)
-            if (i + 1 < argc && argv[i + 1] && argv[i + 1][0] != '-')
-            {
-                value = argv[i + 1];
-                hasValue = true;
-                ++i; // Skip the value in next iteration
-            }
-
-            flags[flagName] = Flag{flagName, value, hasValue};
-        }
-    }
-
-    return flags;
-}
+#include "include/Flags.h"
 
 int main(int argc, char *argv[])
 {
     CommandInvoker commandInvoker;
 
-    auto flags = parseFlags(argc, argv);
+    const FlagMap flags = parseFlags(argc, argv);
 
     for (const auto &[name, flag] : flags)
     {
