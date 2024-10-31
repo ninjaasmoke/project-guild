32e7586d192c16240fd75f23af8dba21543c9a60
for <nithins674@gmail.com>
2024-10-31 22:35:01 +0000

    Update Command class to accept optional flags in execute function

--- include/Command.hpp
+++ include/Command.hpp
@@ -4,12 +4,13 @@
 #include "GetCurrentFolder.hpp"
 #include "CreateGuildFS.hpp"
 #include "CleanBuild.hpp"
+#include "Flags.h"
 
 #include <optional>
 
 class Command
 {
 public:
-    virtual void execute(std::optional<std::string> argument = std::nullopt) = 0;
+    virtual void execute(std::optional<std::string> argument = std::nullopt, std::optional<FlagMap> flags = std::nullopt) = 0;
     virtual ~Command() = default;
 };
