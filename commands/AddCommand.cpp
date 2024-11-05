3ba7e823d8326f103ca1f97ebbff243e2311e6b7
for <nithins674@gmail.com>
2024-11-05 23:10:05 +0000

    Refactor AddCommand.cpp for improved argument handling and URL validation

--- commands/AddCommand.cpp
+++ commands/AddCommand.cpp
@@ -2,22 +2,33 @@
 
 void AddCommand::execute(std::optional<std::string> argument, std::optional<FlagMap> flags)
 {
-
-    if (!argument.has_value() || argument.value().size() == 0)
+    if (!argument.has_value() || argument.value().empty())
     {
-        Logger::error("No argument found! Are you sure you are doing it right?");
+        Logger::error("No argument found! Are you sure you are doing it right?\n");
+        Logger::good("Correct usage:\n\tguild add [resource]\n");
         return;
     }
 
-    // if (!flags.has_value())
-    // {
-    //     Logger::error("No flags provided! Are you sure you are doing it right?");
-    //     return;
-    // }
+    const std::string url = argument.value();
+    if (!utils::url::isValidHttpUrl(url))
+    {
+        Logger::error(url + "\tis an invalid URL!");
+        return;
+    }
 
-    // Logger::info("Hello" + argument.value());
+    // Ensure the packages directory exists
+    std::string packagePath = fs::current_path().string() + "/packages";
+    if (!fs::exists(packagePath))
+    {
+        fs::create_directories(packagePath);
+    }
 
-    
+    FileFetcher fileFetcher(url);
+    if (!fileFetcher.fetchAndSave(packagePath))
+    {
+        Logger::error("Failed to add resource!");
+        return;
+    }
 
-    return;
+    Logger::good("Added resource to '/packages' successfully!");
 }
